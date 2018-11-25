4b81e920de37696398a0124e1f3e382dcda5a5d4
Cezary <cezary.tworzewski@gmail.com>
2018-11-25 17:13:33 +0100

    Add next part code

--- main.js
+++ main.js
@@ -1,44 +1,23 @@
-
 const names = [];
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const div = document.querySelector('div');
 
 const addName = (e) => {
     e.preventDefault();
-    const input = document.querySelector('input');
-    const name = input.value;
-    names.push(name);
-    // console.log(name);
-    const div = document.querySelector('div');
-    div.textContent += name + ', ';
-    input.value = '';
+    const newName = input.value;
+    if (input.value.length) {
+        for (name of names) {
+            if (name === newName) {
+                alert('stop');
+                return;
+            }
+            input.value = '';
+        }
+        names.push(newName);
+        div.textContent += newName + ', ';
+        input.value = '';
+    }
 }
 
-const button = document.querySelector('button');
 button.addEventListener('click', addName);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
